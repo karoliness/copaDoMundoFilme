b99b959fadf1b0b7ce9aaa12346d5cdc0a38a0d9
Karoline de Souza <karolinessilva@live.com>
2020-12-14 09:10:59 -0300

    criação da classe filme

---
+++ Dominio/Filme.cs
@@ -0,0 +1,18 @@
+﻿namespace Dominio
+{
+    public class Filme
+    {
+        public string Id { get; private set; }
+        public string Titulo { get; private set; }
+        public int AnoDeLancamento { get; private set; }
+        public decimal Nota { get; private set; }
+
+        public Filme(string id, string titulo, int anoDeLancamento, decimal nota)
+        {
+            Id = id;
+            Titulo = titulo;
+            AnoDeLancamento = anoDeLancamento;
+            Nota = nota;
+        }
+    }
+}
